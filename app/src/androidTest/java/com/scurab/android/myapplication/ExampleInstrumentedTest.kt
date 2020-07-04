999c987020a89cfd0828e8f07b9bf1149a75c8bb
Jiri Bruchanov <jbruchanov@gmail.com>
2020-07-04 09:37:55 +0100

    Use older versions & ActivityTestRule works

--- app/src/androidTest/java/com/scurab/android/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/scurab/android/myapplication/ExampleInstrumentedTest.kt
@@ -1,11 +1,10 @@
 package com.scurab.android.myapplication
 
-import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,17 +17,15 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
-    @get:Rule
-    var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
+    @get:Rule
+    var activityRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun testDeadLock() {
-        activityRule.scenario.onActivity {
-            val withText = withText("Next")
-            val click = click()
-            val onView = onView(withText)
-            onView.perform(click)
-        }
+    fun testWorking() {
+        val withText = withText("Next")
+        val click = click()
+        val onView = onView(withText)
+        onView.perform(click)
     }
 }
